281ffc99bd7365058e63a7a7205f382b1d4a6084
root <root@vmi3305627.contaboserver.net>
2026-06-07 15:40:32 +0200

    fix bugs

--- velvet-app/vite.config.js
+++ velvet-app/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',   // nginx will serve at /app/
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: '/',
 })
